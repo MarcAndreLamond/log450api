6588889df4aa6be58d3522365e88fe1639df3226
Marc-andré Lamond <marcovich_5@hotmail.com>
2021-04-05 22:35:32 -0400

    change to get only the body

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.post('/orderPosition', (req, res) => {
 
 
 app.post('/order', (req, res) => {
-  const byte = req.body.byte;
+  const byte = req.body;
   const id = req.query.orderId;
 
   // https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd
